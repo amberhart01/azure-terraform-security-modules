b76cd716c7c6995f7c8fe0a9d43c2b865c5fef7b
amberhart01 <amberhart01@icloud.com>
2025-09-28 17:49:24 -0400

    azure_policies: replace null_resource guard with variable validation

--- modules/azure_policies/main.tf
+++ modules/azure_policies/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.0.0, < 5.0.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.2"
+    }
   }
 }
 
